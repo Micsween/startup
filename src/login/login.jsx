af648c9712c63791df8df99c4a70450d698df1a2
Michelle Sweeney <msween@byu.edu>
2025-02-12 11:49:51 -0700

    Finished login component, after some dificulty with conflicting css id names

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./login.css";
 
 export function Login() {
   return (
     <main>
-      <div>login displayed here</div>
+      <div id="login">
+        <img
+          alt="uno"
+          src="https://upload.wikimedia.org/wikipedia/commons/f/f9/UNO_Logo.svg"
+          width="250"
+        />
+        <h1> Welcome to Online Multiplayer Uno!</h1>
+        <form method="get" action="join.html">
+          <div id="login-form">
+            <div>
+              <label for="username">Username:</label>
+              <input type="username" id="username" />
+            </div>
+            <div>
+              <label for="password">Password:</label>
+              <input type="password" id="password" value="password" />
+            </div>
+            <div>
+              <input class="btn btn-danger" type="submit" value="Login" />
+              <input class="btn btn-dark" type="submit" value="Create" />
+            </div>
+            <a href="">Forgot password?</a>
+          </div>
+        </form>
+      </div>
     </main>
   );
-}   
+}
