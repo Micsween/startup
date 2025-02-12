07d012cd30f0dc961ddcc565e7a320af602478b7
Michelle Sweeney <msween@byu.edu>
2025-02-12 11:10:26 -0700

    default react components display nicely. Navigation works fawlessly

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';   
+import { Game } from './game/game';
+import { Join } from './join/join';
+import { Lobby } from './lobby/lobby';
+import { Login } from './login/login';
+import { MatchHistory } from './match-history/match-history';
+
 
 export default function App() {
-    return <div>
+    return (
+    <BrowserRouter>
+    <div>
         <header> 
             <nav className="navbar navbar-expand-lg">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
@@ -11,20 +20,31 @@ export default function App() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <a className="nav-link" href="index.html">Login</a>
-                        <a className="nav-link" href="join.html">Join Game</a>
-                        <a className="nav-link" href="match-history.html">Match History</a>                     
-                        <a className="nav-link" href="lobby.html">Lobby</a>
-                        <a className="nav-link" href="game.html">Game</a>
+                        <NavLink className="nav-link" to="join">Join Game</NavLink>
+                        <NavLink className="nav-link" to="match-history">Match History</NavLink>                     
+                        <NavLink className="nav-link" to="/">Login</NavLink>
+                        <NavLink className="nav-link" to="lobby">Lobby</NavLink>
+                        <NavLink className="nav-link" to="game">Game</NavLink>
                     </div>
                 </div>
             </nav>
         </header>
-        <main className="container-fluid bg-secondary text-center"> App components go here
-        </main>
+        <Routes>
+            <Route path='/' element={<Login />} exact /> 
+            <Route path='/join' element={<Join />} />
+            <Route path='/match-history' element={<MatchHistory />} />
+            <Route path='/lobby' element={<Lobby />} />
+            <Route path='/game' element={<Game />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
         <footer id="footer" className="d-flex flex-wrap justify-content-between align-items-center p-1 px-md-4 mb-2 bg-light border-top">
             <span>Michelle Sweeney is awesome.</span>
             <a className="nav-link" href="https://github.com/Micsween/startup.git">My GitHub!</a>
         </footer>
-    </div>;
-}
+        </div>
+    </BrowserRouter>);
+}   
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center"> There's nothing here. :O </main>;
+  }
