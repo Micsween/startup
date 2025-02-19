be06f2a88b49cd51c1a07198c5ba47c7c11d02d2
Michelle Sweeney <msween@byu.edu>
2025-02-19 13:36:34 -0700

    matchHistory displays but opponents button does not work. working on it

--- src/match-history/match-history.jsx
+++ src/match-history/match-history.jsx
@@ -5,64 +5,105 @@ import "./match-history.css";
 
 /*YOU NEED TO UPATE TIME TAGS TO BE JAVASCRIPT DATE OBJECTS ONCE YOU HAVE YOUR DATABASE*/
 /*Also add a feature where you can route to different matchHistory pages depending on the player you select */
+
+function getOpponentList(opponents) {
+  const opponentList = [];
+  if (opponents != null) {
+    for (const [index, opponentName] of opponents.entries()) {
+      opponentList.push(
+        <li key={opponentName}>
+          <a className="dropdown-item" href="#" >
+            {opponentName}
+          </a>
+        </li>
+      );
+    }
+  }
+  return opponentList;
+}
+
 export function MatchHistory() {
-  const[quote, setQuote] = React.useState("No quote yet");
-  const[quoteAuthor, setQuoteAuthor] = React.useState("idk, some dude.");
-  const[movie, setMovie] = React.useState("From the best movie ever");
+  const [quote, setQuote] = React.useState("No quote yet");
+  const [quoteAuthor, setQuoteAuthor] = React.useState("idk, some dude.");
+  const [movie, setMovie] = React.useState("From the best movie ever");
 
   React.useEffect(() => {
     setQuote("Do or do not. There is no try.");
     setQuoteAuthor("Yoda");
     setMovie("Star Wars");
   }, []);
+
+  const [username, setUsername] = React.useState("Oblivion Phoenix");
+  React.useEffect(() => {
+    setUsername("Autobotkilla");
+  }, []);
+  //should make a match object to make this easier
+  //matches is an array of match objects
+  const [matches, setMatches] = React.useState([
+    {
+      gameName: "The best Game",
+      result: "Win",
+      date: "2/19/2025",
+      oponents: [
+        "Player2's username",
+        "Player3's username",
+        "Player4's username",
+      ],
+    },
+  ]);
+  //const listItems = items.map((item) => 
+  //<li key={item.id}>{item.text}</li>
+//);
+  //update this later to update the values of gameName, result, etc.
+  React.useEffect(() => {
+    //a match has: a game name
+    //a result (win or loss)
+    //a date
+    //a list of opponents
+    //{gameName: "The best Game", result: "Win", date : new Date(2/19/25), oponents: ["Player2's username", "Player3's username", "Player4's username"]}
+  }, []);
+  //replace USERNAMe with the actual username
+
+  const matchHistory = [];
+  if (matches.length) {
+    for (const [index, match] of matches.entries()) {
+      matchHistory.push(
+      <tbody>
+        <tr>
+          <th scope="row"> {match.gameName}</th>
+          <td id="game-win">{match.result}</td>
+          <td>{match.date}</td>
+          <td>
+            {" "}
+            {/* I got rid of a div here so be careful*/}
+            <button
+              className="btn btn-warning dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Opponents
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              {getOpponentList(match.opponents)}
+            </ul>
+          </td>
+        </tr>
+        </tbody>
+      );
+    }
+  } else {
+  }
+
   return (
     <main>
       <div id="content">
-        <h1>Username's Match History</h1>
+        <h1>{username}'s Match History</h1>
         <table id="match-history" className="table table-striped ">
-          <tbody>
-            <tr>
-              <th scope="row"> Game 1</th>
-              <td id="game-win">Win</td>
-              <td>
-                {" "}
-                <time> 1/25/2025</time>
-              </td>
-              <td>
-                <div>
-                  <button
-                    className="btn btn-warning dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Opponents
-                  </button>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton1"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Player2's username
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Player3's username
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Player4's username
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </td>
-            </tr>
-          </tbody>
+
+            {matchHistory}
+    
           <tbody>
             <tr>
               <th scope="row"> Game 2</th>
