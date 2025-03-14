024482507bfed1d25d32b7e4df5679880b511387
Michelle Sweeney <msween@byu.edu>
2025-03-14 17:55:07 -0600

    fixed bug where index.js was missing the code to serve the static files

--- service/index.js
+++ service/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcryptjs";
 import { v4 } from "uuid";
-import { parsePath } from "react-router-dom";
+
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -67,6 +67,7 @@ let lobby2 = {
 let lobbies = [lobby1, lobby2];
 let games = [];
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 let apiRouter = express.Router();
